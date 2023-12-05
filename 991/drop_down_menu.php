8d2500fdc42ac6917caee6152c9dd11bea4bf051
AAKASH JHA <83956789+akashjha991@users.noreply.github.com>
2023-12-05 13:25:06 +0530

    Add files via upload

---
+++ 991/drop_down_menu.php
@@ -0,0 +1,46 @@
+
+<!--first button-->
+
+                                <div class="btn-group">
+  					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>
+  					  Students <span class="caret"></span> 					
+  					</button>
+  				<ul class="dropdown-menu" role="menu">
+  				  <li><a href="everyone.php?tag=student_entry">Students Entry</a></li>
+  				  <li><a href="everyone.php?tag=view_students">View Students</a></li>
+  				</ul>
+				</div>
+				
+				<!--second button-->
+				<div class="btn-group">
+  					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>
+  					  Teachers <span class="caret"></span> 					
+  					</button>
+  				<ul class="dropdown-menu" role="menu">
+  				  <li><a href="everyone.php?tag=teachers_entry">Teachers Entry</a></li>
+  				  <li><a href="everyone.php?tag=view_teachers">View Teachers</a></li>
+  				</ul>
+				</div>
+				
+				<!--third button-->
+				<div class="btn-group">
+  					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-hdd"></span>
+  					  Faculties <span class="caret"></span>
+  					</button>
+  				<ul class="dropdown-menu" role="menu">
+  				  <li><a href="everyone.php?tag=faculties_entry">Faculties Entry</a></li>
+  				  <li><a href="everyone.php?tag=view_faculties">View Faculties</a></li>
+  				</ul>
+				</div>
+				
+				<!--forth button-->
+				<div class="btn-group">
+  					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span>
+  					  Subjects <span class="caret"></span>
+  					</button>
+  				<ul class="dropdown-menu" role="menu">
+  				  <li><a href="everyone.php?tag=subject_entry">Subjects Entry</a></li>
+  				  <li><a href="everyone.php?tag=view_subjects">View Subjects</a></li>
+  				</ul>
+				</div>
+				
---
+++ 991/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>College management system</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/download.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>Institute of Professional Excellence and Management (IPEM) is an affiliated to ccsu university located in Ghaziabad (U.P), India. The university was established in 1996.</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>IPEM Group, under the aegis of Laksh Educational Society, registered under the Societies Act, 1860, continues to build on its reputation as a premier Group of Institutions. At IPEM, we envision a world where your future comes first and we lead with different programs in the areas of Management, Information Technology, Law and Education.
+            UG programs at IPEM, BBA, BCA, B.Com(H) are affiliated to Chaudhary Charan Singh University, Meerut. The B.Ed program is approved by NCTE, Ministry of HRD. PG programs, Masters of Business Administration (MBA) and Master of Computer Applications (MCA) Program are approved by the All India Council for Technical Education (AICTE) and affiliated with Abdul Kalam Technical University (AKTU), Lucknow.</p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
